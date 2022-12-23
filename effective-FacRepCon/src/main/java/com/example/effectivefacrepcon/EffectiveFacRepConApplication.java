246ebf6b18d1aa36e1845077186bdc77ec9e7348
王梓城 <GW00284289@gwm.cn>
2022-12-23 11:01:18 +0800

    Initial commit

---
+++ effective-FacRepCon/src/main/java/com/example/effectivefacrepcon/EffectiveFacRepConApplication.java
@@ -0,0 +1,13 @@
+package com.example.effectivefacrepcon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EffectiveFacRepConApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EffectiveFacRepConApplication.class, args);
+    }
+
+}
---
+++ effective-FacRepCon/src/test/java/com/example/effectivefacrepcon/EffectiveFacRepConApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.effectivefacrepcon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EffectiveFacRepConApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
